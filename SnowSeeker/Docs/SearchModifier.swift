3e7864a329fa9f33ec3e0b3fba5f9538489f2796
A.F. Adib <a.f.adib36@gmail.com>
2024-10-13 19:03:38 +0600

    Searchmodifier

--- SnowSeeker/Docs/SearchModifier.swift
+++ SnowSeeker/Docs/SearchModifier.swift
@@ -8,11 +8,27 @@
 import SwiftUI
 
 struct SearchModifier: View {
+    
     @State private var searchText = ""
+    
+    let allNames = ["Subh", "Vina", "Melvin", "Jack"]
+    
+    var filterNames : [String] {
+        if searchText.isEmpty {
+            allNames
+        } else {
+            allNames.filter { name in
+                name.localizedStandardContains(searchText)
+            }
+        }
+    }
     
     var body: some View {
         NavigationStack {
-            Text("Searching for \(searchText)")
+//            Text("Searching for \(searchText)")
+            List(filterNames, id: \.self) { name in 
+                Text(name)
+            }
                 .searchable(text: $searchText, prompt: "Search Here")
                 .navigationTitle("Searching")
         }
