11ddcf7194f6735e70a83b96785bc5fbcd9d5a9d
A.F. Adib <a.f.adib36@gmail.com>
2024-10-15 02:13:33 +0600

    Resort added

---
+++ SnowSeeker/Resort.swift
@@ -0,0 +1,22 @@
+//
+//  Resort.swift
+//  SnowSeeker
+//
+//  Created by A.F. Adib on 15/10/24.
+//
+
+import Foundation
+
+struct Resort : Codable, Hashable, Identifiable {
+    var id: String
+    var name:  String
+    var country: String
+    var description: String
+    var imageCredit: String
+    var price: Int
+    var size: Int
+    var snowDepth: Int
+    var elevation: Int
+    var runs: Int
+    var facilities: [String]
+}
